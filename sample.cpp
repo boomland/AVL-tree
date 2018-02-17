29ba34703733e2dbd6bab99f00d22f172990f187
Pudyakov Yaroslav <web.programmer.trillioner@gmail.com>
2018-02-17 22:17:50 +0300

    Init

---
+++ sample.cpp
@@ -0,0 +1,19 @@
+#include <string>
+int main() {
+	Set<int> s{10, 1, 5, 3, 6};
+	s.insert(6);
+
+	std::cout << "Initial state: ";
+	for (auto element : s) {
+		std::cout << element << ' ';
+	}
+	std::cout << '\n';
+
+	Iterator<int> it = s.lower_bound(5);
+	std::cout << "Look at the value at the iterator: " << *it << '\n';
+
+	s.clear();
+	std::cout << "Container is empty: " << s.empty() << '\n';
+
+	return 0;
+}
